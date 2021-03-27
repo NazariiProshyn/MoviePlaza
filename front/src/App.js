f8a0e5ba57c6ba51086d6727dd612cc7628ec05b
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-03-27 21:28:04 +0200

    Header component with navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
-import { Route, Switch, Link } from 'react-router-dom';
+
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
 import Catalog from './pages/catalog';
 import Room from './pages/room';
 import User from './pages/user';
+import Header from './components/Header/Header';
 function App() {
     return (
         <div className="App">
-            <ul className="header">
-                <li>
-                    <Link to="/">MoviePlaza</Link>
-                </li>
-                <li>
-                    <Link to="/catalog">Фільми</Link>
-                </li>
-                <li>
-                    <Link to="/room">Створити кімнату</Link>
-                </li>
-                <li>
-                    <input type="search" placeholder="Пошук фільму"></input>
-                    <input type="submit"></input>
-                </li>
-                <li>
-                    <Link to="/user">
-                        money
-                        <img src="../images/user.png" alt="user"></img>
-                    </Link>
-                </li>
-            </ul>
+            <Header></Header>
             <div className="content">
                 <Switch>
                     <Route exact path="/" component={Home} />
---
+++ front/src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './Header.css';
+import user from './../../images/user.png';
+import logo from './../../images/Logo.png';
+import searchimg from './../../images/searc.png';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+    render() {
+        return (
+            <ul className="header">
+                <li>
+                    <Link to="/" className="navlink">
+                        <img src={logo} alt="logo" id="logoImg"></img>
+                        MoviePlaza
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/catalog" className="navlink">
+                        Фільми
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/room" className="navlink">
+                        Створити кімнату
+                    </Link>
+                </li>
+                <li>
+                    <input
+                        id="filmsearch"
+                        type="search"
+                        placeholder="Пошук фільму"
+                    ></input>
+                    {/*добавити onclick для img або замінити img на button i знайти спосіб як поставити на фон кнопки картинку */}
+                    <img src={searchimg} alt="search" id="searchImg"></img>
+                </li>
+                <li>
+                    <Link to="/balancerefill" className="navlink">
+                        Поточний рахунок:
+                    </Link>
+                    <div id="balance">0 грн</div>
+                </li>
+                <li>
+                    <Link to="/user">
+                        <img src={user} alt="user" id="userImg"></img>
+                        {/* Якщо виконано вхід на аккаунт то замість цього тексту буде нік користувача*/}
+                        <div id="enter">Увійти/зареєструватися</div>
+                    </Link>
+                </li>
+            </ul>
+        );
+    }
+}
+
+export default Header;
