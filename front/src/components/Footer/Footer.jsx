3e779d6122bb0518974ac46ffe1ce039e252a181
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2021-03-30 21:20:51 +0300

    fix names in footer

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './Footer.css';
+import f from './Footer.module.css';
+
 import logo from './../../images/Logo.png';
 import telegram from './../../images/telegram.png';
 import twitter from './../../images/twitter.png';
@@ -10,29 +11,29 @@ class Footer extends Component {
     render() {
         return (
             <footer>
-                <img src={logo} alt="logo" id="logofoot"></img>
-                <div id="footerplaza">MoviePlaza</div>
-                <div id="footerdesc">
+                <img src={logo} alt="logo" id={f['footer-logo']}></img>
+                <div id={f['footer-title']}>MoviePlaza</div>
+                <div id={f['footer-description']}>
                     Created in 2021 by MoviePlaza Teams. All rights reserved and
                     confirmed. Criminal liability is provided for the creation
                     of fakes and the use of site resources. For cooperation call
                     +38000000000 or email movieplaza@gm.com
                 </div>
-                <div className="contacts">
-                    <div id="contacttext">Our Contacts</div>
-                    <div id="telegram">
+                <div className={f.contacts}>
+                    <div id={f['contact-title']}>Our Contacts</div>
+                    <div id={f.telegram}>
                         <img src={telegram} alt="tel" id="telImg"></img>
                         <div>user.acc</div>
                     </div>
-                    <div id="instagram">
+                    <div id={f.instagram}>
                         <img src={instagram} alt="inst" id="instImg"></img>
                         <div>user.acc</div>
                     </div>
-                    <div id="link">
+                    <div id={f.link}>
                         <img src={link} alt="link" id="linkImg"></img>
                         <div>user.acc</div>
                     </div>
-                    <div id="twitter">
+                    <div id={f.twitter}>
                         <img src={twitter} alt="twit" id="twitImg"></img>
                         <div>user.acc</div>
                     </div>
