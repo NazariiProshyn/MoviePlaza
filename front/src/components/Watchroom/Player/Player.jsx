d30383d517c5fadbf02d4e99004197070c8816a3
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2021-04-01 03:51:41 +0300

    update on Watchroom component structure

---
+++ front/src/components/Watchroom/Player/Player.jsx
@@ -0,0 +1,9 @@
+import p from './Player.module.css';
+
+function Player(params) {
+    return (
+        <div className={p['watchroom-player']}></div>
+    );
+}
+
+export default Player;
--- front/src/components/Watchroom/Watchroom.jsx
+++ front/src/components/Watchroom/Watchroom.jsx
@@ -1,37 +1,12 @@
-
 import w from './Watchroom.module.css';
-import user_img from '../../images/user.png';
-
+import Chat from './Chat/Chat';
+import Player from './Player/Player';
 
 function Watchroom(params) {
     return (
         <div className={w.watchroom}>
-            <div className={w['watchroom-player']}></div>
-            <div className={w['watchroom-chat']}>
-                <div className={w['chat-buttons']}>
-                    <button className={w['chat-buttons__copy']} type="button">Скопировать ссылку</button>
-                </div>
-
-                <div className={w['chat-messages']}>
-                    <div className={w.message}>
-                        <div className={w['message-avatar']}>
-                            <img className={w['message-avatar__image']} src={user_img} alt="avatar"/>
-                        </div>
-                        <div className={w['message-content']}>
-                            <div className={w['message-content__username']}>
-                                <a className={w['link-to-user']} href="/">username</a>
-                                <small className={w['current-time']}>22:50</small>
-                            </div>
-                            <div className={w['message-content__text']}>message text</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={w['chat-input']}>
-                    <input className={w['chat-input__input']} type="text" placeholder="Введите сообщение" data-emojiable="converted"></input>
-                    <button className={w['chat-input__send']} type="button">Отправить</button>
-                </div>          
-            </div>
+            <Player/>
+            <Chat/>
         </div>
     );
 }
