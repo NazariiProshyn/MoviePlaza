057f2b304c9bf09cf524548f47cae2529e60bbac
NazariiProshyn <nazariyproshyn@gmail.com>
2021-04-11 01:15:54 +0300

    Add desription

--- back/src/DateBase/MoviePlazaDB.sql
+++ back/src/DateBase/MoviePlazaDB.sql
@@ -1,15 +1,42 @@
  /*******************************
- * FILENAME:      MoviePlaza.sql 
+ *
+ * FILENAME:         MoviePlaza.sql 
  * 
- * BRIEF:         This file contains product, customer
- *                data and realization of database for
- *                github.com/NazariyProshyn/MoviePlaza
+ * BRIEF:            This file contains product, customer
+ *                   data and realization of database for
+ *                   github.com/NazariyProshyn/MoviePlaza
  *
- * AUTHOR:        Nazarii Proshyn
+ * AUTHOR:           Nazarii Proshyn
  *
- * CONTACT:       nazariyproshyn@gmail.com
+ * CONTACT:          nazariyproshyn@gmail.com
  *
  *******************************/
+ 
+ /******************************
+ *
+ * DB name:             MoviePlaza
+ *
+ * BRIEF:               Contains information about
+ *                      customers, films, transactions
+ *                      of "MoviePlaza" service
+ *
+ * TABLES:              "User",       "UserInformation"
+ *
+ * Description:
+ * "User":              This table stores the main    
+ *                      information about the client-
+ *                      date of birth, name, surname,
+ *                      money in the account.The "UserId"
+ *                      column is taken as the primary key.
+ *
+ * "UserInformation":   This table has information about the
+ *                      client's account - his login and 
+ *                      password.The "UserId" column is taken
+ *                      as the primary key.The "UserId" column
+ *                      in the "User" table is taken as the foreign key.
+ *
+ ******************************/
+ 
  
  /*
   *BRIEF: Creating of datebase
@@ -22,11 +49,11 @@
   *
   */
  CREATE TABLE "User" (
-	"UserId" serial NOT NULL,
-	"Name" varchar(255) NOT NULL,
+	"UserId"     serial NOT NULL,
+	"Name"       varchar(255) NOT NULL,
 	"SecondName" varchar(255) NOT NULL,
-	"BDate" DATE NOT NULL,
-	"Money" money,
+	"BDate"      DATE NOT NULL,
+	"Money"      money,
 	CONSTRAINT "User_pk" PRIMARY KEY ("UserId")
  ) WITH (
   OIDS=FALSE
@@ -37,8 +64,8 @@
   *
   */
  CREATE TABLE "UserInformation" (
-	"UserId" bigint NOT NULL,
-	"Login" varchar(255) NOT NULL UNIQUE,
+	"UserId"   bigint NOT NULL,
+	"Login"    varchar(255) NOT NULL UNIQUE,
 	"Password" varchar(255) NOT NULL,
 	CONSTRAINT "UserInformation_pk" PRIMARY KEY ("UserId")
 ) WITH (
