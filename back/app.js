accf6ad6c6a95a10b18b00f09ce5c4c6c69317bf
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2021-04-12 18:33:16 +0300

    chat working ayyeee

--- back/app.js
+++ back/app.js
@@ -1,60 +1,66 @@
-const fastify = require('fastify')({
-    logger: true,
-});
+const fastify = require('fastify');
+
+const app = fastify({ logger: true })
+app.register(require('fastify-socket.io'), {
+  cors: {origin: "*"}
+})
 const fastifySession = require('fastify-session');
 const fastifyCookie = require('fastify-cookie');
-fastify.register(fastifyCookie);
+
+app.register(fastifyCookie);
+
 let sessionstore = new fastifySession.MemoryStore();
-fastify.register(fastifySession, {
-    store: sessionstore,
-    cookieName: 'sessionId',
-    secret: '1qwqwqwwhjehu2372e8ywhdhu92e8uids',
-    cookie: { secure: false, path: '/' },
-    expires: 900000,
+app.register(fastifySession, {
+  store: sessionstore,
+  cookieName: 'sessionId',
+  secret: '1qwqwqwwhjehu2372e8ywhdhu92e8uids',
+  cookie: { secure: false, path: '/' },
+  expires: 900000,
 });
 
-fastify.register(require('fastify-cors'), {
-    origin: 'http://localhost:3000',
+app.register(require('fastify-cors'), {
+  origin: 'http://localhost:3000',
 
-    credentials: 'same-origin',
-    allowMethods:
-        'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST',
-    allowedHeaders: [
-        'Content-Type',
-        'Authorization',
-        'Cache-Control',
-        'X-Custom-Header',
-        'X-Requested-With',
-        'Cookie',
-    ],
+  credentials: 'same-origin',
+  allowMethods:
+      'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST',
+  allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Cache-Control',
+      'X-Custom-Header',
+      'X-Requested-With',
+      'Cookie',
+  ],
 });
 //fastify.register(routes);
-fastify.register(require('./routes/index'));
-fastify.register(require('./routes/login'));
-fastify.register(require('./routes/filmImg'));
-fastify.register(require('./routes/films'));
-fastify.register(require('./routes/users'));
-fastify.register(require('./routes/videos'));
+app.register(require('./routes/index'));
+app.register(require('./routes/login'));
+app.register(require('./routes/filmImg'));
+app.register(require('./routes/films'));
+app.register(require('./routes/users'));
+app.register(require('./routes/videos'));
 
 
-const io = require('socket.io')(fastify.server);
+app.ready(err => {
+  if (err) throw err
+  app.io.on('connection', (socket) => {
+    console.log('Socket connected!');
 
-io.on('connection', function (socket) {
-    console.log('server connected');
-    
-    socket.on('disconnect', function () {
-        console.log('server disconnected');
-    });
+    socket.on('chat_message', (message) => {
+      console.log(message);
+      app.io.emit('chat_message', socket.id.substr(0,2), message);
+    })
+
 });
+  
 
 
+})
 
-const start = async () => {
-    try {
-        await fastify.listen(3001);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-};
-start();
+app.listen(3001, (err) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+})
--- front/src/components/Watchroom/Watchroom.jsx
+++ front/src/components/Watchroom/Watchroom.jsx
@@ -1,23 +1,133 @@
+import React, { useEffect } from 'react';
 import w from './Watchroom.module.css';
-import Chat from './Chat/Chat';
+
+import user_img from '../../images/user.png';
 import Player from './Player/Player';
 
 import { io } from 'socket.io-client';
-const socket = io.connect();
 
 function Watchroom(params) {
-    socket.on('connection', () => {
-        console.log(`Client connected: ${socket.id}`);
-    });
+    const ENDPOINT = 'localhost:3001';
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+    useEffect(() => {
+        const socket = io.connect(ENDPOINT);
+        socket.on('connect', () => {
+            console.log(`Client connected: ${socket.id}`);
+        });
+
+        socket.on('chat_message', (username, text) => {
+            const today = new Date();
+            const time = today.getHours() + ':' + today.getMinutes();
+
+            const message = document.createElement('div');
+            message.className = w.message;
+
+            const message_avatar = document.createElement('div');
+            message_avatar.className = w['message-avatar'];
+
+            const message_avatar_image = document.createElement('img');
+            message_avatar_image.className = w['message-avatar__image'];
+            message_avatar_image.src = user_img;
+            message_avatar_image.alt = 'avatar';
+            message_avatar.appendChild(message_avatar_image);
+            message.appendChild(message_avatar);
+
+            const message_content = document.createElement('div');
+            message_content.className = w['message-content'];
+
+            const username_div = document.createElement('div');
+            username_div.className = w['message-content__username'];
+
+            const anchor = document.createElement('a');
+            anchor.className = w['link-to-user'];
+            anchor.href = '/';
+            anchor.innerText = username;
+            username_div.appendChild(anchor);
+
+            const small = document.createElement('small');
+            small.className = w['current-time'];
+            small.innerText = time;
+            username_div.appendChild(small);
+
+            message_content.appendChild(username_div);
+
+            const message_text = document.createElement('div');
+            message_text.className = w['message-content__text'];
+            message_text.innerText = text;
+            message_content.appendChild(message_text);
+            message.appendChild(message_content);
+
+            document.querySelector('[data-messages]').append(message);
+
+            console.log(text);
+        });
+
+        document.getElementById('chat-button').onclick = () => {
+            const text = document.getElementById('chat-input').value;
+            document.getElementById('chat-input').value = '';
+            socket.emit('chat_message', text);
+        };
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnected');
+        });
+    }, [ENDPOINT]);
 
     return (
         <div className={w.watchroom}>
             <Player />
-            <Chat />
+            <div className={w['watchroom-chat']}>
+                <div className={w['chat-buttons']}>
+                    <button className={w['chat-buttons__copy']} type="button">
+                        Скопировать ссылку
+                    </button>
+                </div>
+
+                <div
+                    className={w['chat-messages']}
+                    data-messages="messages-holder"
+                >
+                    <div className={w.message}>
+                        <div className={w['message-avatar']}>
+                            <img
+                                className={w['message-avatar__image']}
+                                src={user_img}
+                                alt="avatar"
+                            />
+                        </div>
+                        <div className={w['message-content']}>
+                            <div className={w['message-content__username']}>
+                                <a className={w['link-to-user']} href="/">
+                                    username
+                                </a>
+                                <small className={w['current-time']}>
+                                    22:50
+                                </small>
+                            </div>
+                            <div className={w['message-content__text']}>
+                                message text
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={w['chat-input']}>
+                    <input
+                        className={w['chat-input__input']}
+                        type="text"
+                        placeholder="Введите сообщение"
+                        data-emojiable="converted"
+                        id="chat-input"
+                    ></input>
+                    <button
+                        className={w['chat-input__send']}
+                        type="button"
+                        id="chat-button"
+                    >
+                        Отправить
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
