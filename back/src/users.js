df4c17a93f427aab1d169114d226079c5ac7706a
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-04-28 04:48:30 +0300

    added removal of empty rooms from db

--- back/src/users.js
+++ back/src/users.js
@@ -26,6 +26,11 @@ async function getCurrentUser(id) {
 // User leaves chat
 async function userLeave(id) {
     const user = await pool.query('SELECT * FROM "UsersRoom" WHERE socketId = $1', [id]);
+    const amountofuser = await pool.query('SELECT COUNT(*) FROM "UsersRoom" WHERE room = $1', [user.rows[0].room]);
+    console.log(amountofuser.rows[0]);
+    if (amountofuser.rows[0].count==='1'){
+        await pool.query('DELETE FROM "Rooms" WHERE roomId = $1', [user.rows[0].room]);
+    }
     console.log(user.rows[0]);
     await pool.query('DELETE FROM "UsersRoom" WHERE socketId = $1', [id]);
     return user.rows[0];
